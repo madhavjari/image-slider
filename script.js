605f9cf289c14b1bb2bd74535cc239aad54ab322
madhavjari <213913746+madhavjari@users.noreply.github.com>
2025-09-26 12:43:42 +0530

    created functions

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const leftBtn = document.querySelector('.left');
 const rightBtn = document.querySelector('.right');
 
-leftBtn.addEventListener('click', () =>{
+function gettingCurrentImage(){
     let count = 0;
-    const currPic = document.querySelector('.images > .show');
+    const currPic = document.querySelector('.show');
     const allImg = document.querySelectorAll('img');
     const dataId = parseInt(currPic.dataset.id);
 
     const currNum = document.querySelector('.number > .show');
     const numberDataId = parseInt(currNum.dataset.id);
 
-    allImg.forEach(image => {
+    allImg.forEach(() => {
         count++;
     })
-    if(dataId === 1 && numberDataId === 1){
+    return {currPic,currNum,dataId,numberDataId,count};
+}
+
+function changingImage(count,previousDataId,changeDataId,currPic,currNum){
+    if(previousDataId === 1 && previousDataId > changeDataId){
         const showPic = document.querySelector(`img[data-id="${count}"]`);
         showPic.classList.remove('hide');
         showPic.classList.add('show');
@@ -26,48 +30,7 @@ leftBtn.addEventListener('click', () =>{
         currNum.classList.remove('show');
         currNum.classList.add('hide');
     }
-    else{
-        const showPic = document.querySelector(`img[data-id="${dataId-1}"]`)
-        showPic.classList.remove('hide');
-        showPic.classList.add('show');
-        currPic.classList.remove('show');
-        currPic.classList.add('hide');
-
-        const showNum = document.querySelector(`.number>div[data-id="${dataId-1}"]`);
-        showNum.classList.remove('hide');
-        showNum.classList.add('show');
-        currNum.classList.remove('show');
-        currNum.classList.add('hide');
-    }
-    count = 0;
-})
-
-rightBtn.addEventListener('click', () =>{
-    let count = 0;
-    const currPic = document.querySelector('.show');
-    const allImg = document.querySelectorAll('img');
-    const dataId = parseInt(currPic.dataset.id);
-
-    const currNum = document.querySelector('.number > .show');
-    const numberDataId = parseInt(currNum.dataset.id);
-
-    allImg.forEach(() => {
-        count++;
-    })
-    if(dataId === count && numberDataId === count){
-        const showPic = document.querySelector(`img[data-id="1"]`);
-        showPic.classList.remove('hide');
-        showPic.classList.add('show');
-        currPic.classList.remove('show');
-        currPic.classList.add('hide');
-
-        const showNum = document.querySelector(`.number>div[data-id="1"]`);
-        showNum.classList.remove('hide');
-        showNum.classList.add('show');
-        currNum.classList.remove('show');
-        currNum.classList.add('hide');
-    }
-    else{
+    else if (previousDataId === count && changeDataId > previousDataId){
         const showPic = document.querySelector(`img[data-id="${dataId+1}"]`);
         showPic.classList.remove('hide');
         showPic.classList.add('show');
@@ -80,5 +43,22 @@ rightBtn.addEventListener('click', () =>{
         currNum.classList.remove('show');
         currNum.classList.add('hide');        
     }
-    count = 0;
+}
+
+leftBtn.addEventListener('click', () =>{
+    const currentImage =  gettingCurrentImage();
+
+    const count = currentImage.count;
+    const previousDataId = currentImage.dataId;
+    let changeDataId = previousDataId + 1;
+    const currPic = currentImage.currPic;
+    const currNum = currentImage.currNum;
+
+    changingImage(count,previousDataId,changeDataId,currPic,currNum);
+
+    gettingCurrentImage.count = 0;
 })
+
+//rightBtn.addEventListener('click', slide);
+
+setInterval(slide,2000);
